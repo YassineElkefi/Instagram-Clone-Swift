b89cf34dcfb1e234dfef14ff093e7b146b740cb8
YassineElkefi <yassine.elkefi6@gmail.com>
2025-02-23 21:05:50 +0100

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Yassine EL KEFI on 23/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
